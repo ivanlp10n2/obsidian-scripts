1abf44645180508c9cb14c080e08993587cc60c3
ivan <ivan@empanada.local>
2024-07-01 09:13:54 -0300

    updated files

--- base for migration.md
+++
@@ -1,1 +0,0 @@
-<% tp.user.rename_date_format_files_all_folder({tp, app}, '🎁 life/📆 daily/📓 notes/') %>
--- test.js
+++
@@ -1,9 +0,0 @@
-const test = async () => {
-    new Promise((resolve, reject) => {
-        console.log("test async function")
-        resolve("test async function")
-    })
-    return "test async function"
-}
-
-module.exports = test;
